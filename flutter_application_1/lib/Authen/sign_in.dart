d1f96f6ff8de45da94bb2f7412999a3dc5d75b92
Siravet <sittikorn146@gmail.com>
2025-10-07 14:49:19 +0700

    fix2

--- flutter_application_1/lib/Authen/sign_in.dart
+++ flutter_application_1/lib/Authen/sign_in.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import '../widgets/snackbar.dart';
+import 'services/snackbar.dart';
 import '../Homepage/home.dart';
 import 'services/auth_service.dart';
 
